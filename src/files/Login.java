95a90a39fb9a2e88db57c4e31a5fca31653e9500
juliamcclellan <jmcclellan@mxschool.edu>
2017-11-12 01:35:34 -0500

    Importing jbcrypt

--- src/files/Login.java
+++ src/files/Login.java
@@ -1,13 +1,28 @@
 package files;
 
+import org.mindrot.jbcrypt.BCrypt;
+
 import java.util.ArrayList;
 
 import grades.ClassData;
 
 public class Login
 {
+	/**
+	 * Attempts to log in a user
+	 * @param username The user's entered username
+	 * @param password The user's entered password
+	 * @return The user's class data if login was successful (an empty arraylist if 
+	 * there is no data for the user), or null if the user does not exist or entered
+	 *  an incorrect password
+	 */
 	public static ArrayList<ClassData> login(String username, String password)
 	{
 		return ImportExport.importData(); // to be implemented later
+	}
+	
+	public static boolean newUser(String username, String password)
+	{
+		BCrypt.hashpw(password, BCrypt.gensalt();
 	}
 }
