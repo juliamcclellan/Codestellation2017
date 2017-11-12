1c9328e0d3c4d2dc998387b0cd66277d81ac913c
juliamcclellan <jmcclellan@mxschool.edu>
2017-11-11 21:21:32 -0500

    Login stub

---
+++ src/files/Login.java
@@ -0,0 +1,13 @@
+package files;
+
+import java.util.ArrayList;
+
+import grades.ClassData;
+
+public class Login
+{
+	public static ArrayList<ClassData> login(String username, String password)
+	{
+		return ImportExport.importData(); // to be implemented later
+	}
+}
